de1d70884eb8e97cbc7342f8374c0f39d917485c
Ben B <benbuzzee@gmail.com>
2023-07-09 15:13:29 -0400

    add time since event script

---
+++ R/time_since_event_dt.R
@@ -0,0 +1,57 @@
+# time-since-event
+
+# The goal of this script is to create a new variable in our dataset
+# that represents the time since an event has occurred
+
+
+library(lubridate)
+library(data.table)
+library(tidyverse)
+
+
+#=====================
+# Create dummy data
+#=====================
+
+# Each row is a patient ID, a date, and an event indicator variable
+# Note: person 2 has two events
+df <- data.frame(id = rep(1:2, each = 10),
+                 date = 20200101:20200120,
+                 event_indicator = rep(0, times = 20))
+
+df$event_indicator[c(5, 15, 18)] <- 1
+
+head(df)
+
+#=============================================
+# Step 1: Create a variable that is the date 
+# when the event occurs and missing otherwise
+# Note: This only seems to work for integer dates
+#=============================================
+
+df <- df %>% mutate(event_date = ifelse(event_indicator == 1, date, NA_integer_))
+
+#=========================================
+# Step 2: use data.table's nafill function 
+# to create a new variable that represents
+# the last non-missing value, carried forward from the event on.
+#========================================
+
+dt <- setDT(df)[, last_event_date := (nafill(event_date, "locf")), by = id]
+
+
+#========================================================
+# Step 3: Convert to dates using lubridate then subtract
+#========================================================
+
+dt$date <- ymd(dt$date)
+dt$last_event_date <- ymd(dt$last_event_date)
+
+
+time_since_event_dt <- new_dt[, time_since_last_event := date - last_event_date,]
+
+#=====================================================
+# Now our final dataset has a variable that tracks how
+# long it's been since the last event, and NA values
+# indicate that an event has not occurred yet.
+#======================================================
--- README.md
+++ README.md
@@ -4,8 +4,9 @@ Useful R and SAS code/functions for the analysis of insurance claims data. Each 
 
 ##### **R**
 
-  - **accumulate.R** This is an application of the accumulate() function from the purrr package that creates a cumulative variable (often of time) that resets after a certain amount of time has passed. Originally used to identify birth-related encounters that were 6 months apart.
-
+  - **accumulate.R** This is an application of the accumulate() function from the purrr package that creates a cumulative variable (often of time) that resets after a certain amount of time accumulates. Originally used to identify birth-related encounters that were 6 months apart.
+  - **time_since_event_dt.R** Creates a variable that represents how much time (days) has
+  occurred since the last event takes place using data.table's nafill() function. OG purpose: Calculate the time since an opioid use disorder encounter for OUD patients.
 
 ##### **SAS**
  - **proc_sql_into** This script extracts a variable from a data set and turns it into a macro variable. Originally used when we had an indicator that a medication/code was used for a certain reason in one data set and we wanted to search another data set for observations with those same codes.
